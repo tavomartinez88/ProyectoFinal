3da05a99d036f22ff5345952053b58ccef9d3490
gustavo <gmartinezgranella@gmail.com>
2014-09-08 02:53:36 -0300

    added insert,search and delete of complejos

--- Pruebas/complejos/views.py
+++ Pruebas/complejos/views.py
@@ -1,2 +1,39 @@
-from django.shortcuts import render
-# Create your views here.
+from django.shortcuts import render_to_response, redirect
+from django.http import HttpResponse
+
+from django.template import RequestContext
+from complejos.models import Complejo
+from django.db.models import Q
+from forms import ComplejoForm
+from django.http import HttpResponseRedirect
+from django.core.context_processors import csrf
+def search_form(request):
+  query = request.GET.get('q', '')
+  if query:
+	qset = (Q(nombre__icontains=query))
+	results = Complejo.objects.filter(qset).distinct()
+
+  else:
+	results = []
+  return render_to_response("search_form.html",{"results": results,"query": query})
+
+
+def crear_form(request):
+    nombre = request.GET.get('nombre', '')
+    direccion = request.GET.get('direccion', '')
+    telefono = request.GET.get('telefono', '')
+    nombreTitular = request.GET.get('nombreTitular', '')
+    res = Complejo(nombre=nombre, direccion=direccion, telefono=telefono, nombreTitular=nombreTitular)
+    res.save()
+    return render_to_response("crear_form.html",{"res": res,"query": query})
+
+def eliminar_form(request):
+  query = request.GET.get('q', '')
+  if query:
+	qset = (Q(nombre__icontains=query))
+	results = Complejo.objects.filter(qset).distinct()
+	res=results.delete()
+
+  else:
+	res = []
+  return render_to_response("search_form.html",{"res": res,"query": query})
