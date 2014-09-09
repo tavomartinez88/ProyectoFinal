fe412403b20db8ce304d8b2c7f460e697c9e72ec
Javier Furlán <javifurlan@hotmail.com>
2014-09-09 09:16:31 -0300

    Added ABM jugadores

--- Pruebas/jugadores/views.py
+++ Pruebas/jugadores/views.py
@@ -1,4 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response, redirect
+from django.http import HttpResponse
+from jugadores.models import jugadores
+from django.db.models import Q
+from django.http import HttpResponseRedirect
+from django.core.context_processors import csrf
+from django import forms
 
-# Create your views here.
+def search_player(request):
+   query = request.GET.get('q', '')
+   if query:
+      qset = (Q(nombre__icontains=query) |
+      Q(apellido__icontains=query) |
+      Q(dni__contains=query))
+      results = jugadores.objects.filter(qset).distinct()
+   else:
+      results = []
+   return render_to_response("search_player.html", {"results": results,"query": query})
+
+def registrar_jugador(request):
+    nombre = request.GET.get('nombre', '')
+    apellido = request.GET.get('apellido', '')
+    dni = request.GET.get('dni', 0)
+    email = request.GET.get('email', '')
+    direccion = request.GET.get('direccion', '')
+    telefono1 = request.GET.get('telefono1', '')
+    telefono2 = request.GET.get('telefono2', '')
+    username = request.GET.get('username', '')
+    password = request.GET.get('password', '')
+    res = jugadores(nombre=nombre, apellido=apellido, dni=dni, email=email, direccion=direccion, tel1=telefono1, tel2=telefono2, username=username, password=password) 
+    res.save()
+    return render_to_response("registrar_jugador.html",{"res": res})
+
+def eliminar_jugador(request):
+  query = request.GET.get('q', '')
+  if query:
+      qset = (Q(nombre__icontains=query) |
+      Q(apellido__icontains=query) |
+      Q(dni__contains=query))
+      results = jugadores.objects.filter(qset).distinct()
+      res=results.delete()
+  else:
+	res = []
+  return render_to_response("search_player.html",{"res": res,"query": query})
 
--- Pruebas/mysite/urls.py
+++ Pruebas/mysite/urls.py
@@ -8,4 +8,7 @@ urlpatterns = patterns('',
     					url(r'^admin/', include(admin.site.urls)),
     					url(r'^search-form/', 'complejos.views.search_form', name='search_form'),
     					url(r'^crear-form/', 'complejos.views.crear_form', name='crear_form'),
-    					url(r'^eliminar-form/', 'complejos.views.eliminar_form', name='eliminar_form'),)
+    					url(r'^eliminar-form/', 'complejos.views.eliminar_form', name='eliminar_form'),
+    					url(r'^search-player/', 'jugadores.views.search_player', name='search_player'),
+    					url(r'^registro/', 'jugadores.views.registrar_jugador', name='registrar_jugador'),
+    					url(r'^eliminar-jugador/', 'jugadores.views.eliminar_jugador', name='eliminar_jugador'),)
