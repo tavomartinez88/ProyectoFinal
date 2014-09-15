552dcb22deb53190838dfc33a9f8e03e91054d75
Javier Furlán <javifurlan@hotmail.com>
2014-09-15 01:01:49 -0300

    Modified urls.py

--- Pruebas/mysite/urls.py
+++ Pruebas/mysite/urls.py
@@ -5,10 +5,15 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    					url(r'^admin/', include(admin.site.urls)),
-    					url(r'^search-form/', 'complejos.views.search_form', name='search_form'),
-    					url(r'^crear-form/', 'complejos.views.crear_form', name='crear_form'),
-    					url(r'^eliminar-form/', 'complejos.views.eliminar_form', name='eliminar_form'),
-    					url(r'^search-player/', 'jugadores.views.search_player', name='search_player'),
-    					url(r'^registro/', 'jugadores.views.registrar_jugador', name='registrar_jugador'),
-    					url(r'^eliminar-jugador/', 'jugadores.views.eliminar_jugador', name='eliminar_jugador'),)
+	    				url(r'^admin/', include(admin.site.urls)),)
+
+urlpatterns += patterns('complejos.views',
+    					url(r'^search-form/', 'search_form', name='search_form'),
+    					url(r'^crear-form/', 'crear_form', name='crear_form'),
+    					url(r'^eliminar-form/', 'eliminar_form', name='eliminar_form'),)
+
+urlpatterns += patterns('jugadores.views',
+	    				url(r'^search-player/', 'search_player', name='search_player'),
+    					url(r'^registro/', 'registrar_jugador', name='registrar_jugador'),
+    					url(r'^eliminar-jugador/', 'eliminar_jugador', name='eliminar_jugador'),
+    					url(r'^players/', include('jugadores.urls', namespace="upersonas")),)
