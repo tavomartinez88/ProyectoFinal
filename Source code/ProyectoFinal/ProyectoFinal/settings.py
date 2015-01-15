8fea08cd82d66a7201fc8b749b899a672652833c
gustavo <gustavo@gustavo-HP-G42-Notebook-PC.(none)>
2015-01-15 19:29:58 -0300

    [Finish #86147750] create django project

--- Source code/ProyectoFinal/ProyectoFinal/settings.py
+++ Source code/ProyectoFinal/ProyectoFinal/settings.py
@@ -1,5 +1,5 @@
 """
-Django settings for ProyectoFinal project.
+Django settings for ProyectoFinal project...
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/topics/settings/
