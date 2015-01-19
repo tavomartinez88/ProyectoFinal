787a6647c0ff97d1fc5566f8d113244528de42f2
Javier Furlán <javifurlan@hotmail.com>
2015-01-19 17:41:57 -0300

    Removed duplicated folder

--- Source code/citys/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from citys.models import City 
-
-admin.site.register(City)
-
-# Register your models here.
--- Source code/citys/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render_to_response, redirect
-from django.http import HttpResponse
-from django.template import RequestContext
-from citys.models import City
-from django.db.models import Q
-from django.http import HttpResponseRedirect
-from django.core.context_processors import csrf
-
