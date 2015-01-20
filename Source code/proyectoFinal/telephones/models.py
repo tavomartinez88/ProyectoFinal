16d8b58e086c1446335a83ca89a1bb8aa5551ae4
gustavo <gustavo@gustavo-HP-G42-Notebook-PC.(none)>
2015-01-20 17:58:15 -0300

    [delivers #86155572 #86155402] modify models of telephones and added operations of model

--- Source code/proyectoFinal/telephones/models.py
+++ Source code/proyectoFinal/telephones/models.py
@@ -4,21 +4,40 @@ from django.db import models
 class Telephone(models.Model):
 	number = models.CharField(max_length=9)
 
-	def createTelephone(self, numero):
-		num = Telephone(number = numero)
-		num.save()	
-
-
 
 class LandLine(models.Model):
 	areaCode = models.CharField(max_length=6)
 	Id_number = models.ForeignKey(Telephone)
 
-	def createLandLine(self, codeArea, numero):
-		cod = LandLine(areaCode = codeArea)
-		cod.save()
+	def createLandLine(self, numero, area):
 		num = Telephone(number = numero)
 		num.save()
+		fijo = LandLine(Id_number = num.id, areaCode = area)
+		fijo.save()
+		
+
+	def deleteLandLine(self, numero, area):
+		num = Telephone.objects.get(number = numero, areaCode = area)
+		num.delete()
+
+	def updateLandLine(self, numberNow, numberNew,areaNow, areaNew):
+		num = Telephone.objects.get(number = numberNow)
+		cod = LandLine.objects.get(Id_number = num.id, areaCode = areaNow)
+		
+		if numberNew != "" && areaNew!= "" && num != null && cod != null :
+			cod.areaCode = areaNew
+			num.number = numberNew
+			num.save()
+			cod.save()
+		
+		if numberNew == "" && areaNew!= "" && num != null && cod != null :
+			cod.areaCode = areaNew
+			cod.save()			
+
+		if numberNew != "" && areaNew == "" && num != null && cod != null :
+			num.number = numberNew
+			num.save()
+
 
 
 	def __str__(self):
@@ -34,7 +53,20 @@ class CellPhone(models.Model):
 
 	def createCellPhone(self,numero):
 		num = Telephone(number = numero)
+		cell = CellPhone(Id_number = num.id)
 		num.save()
+		cell.save()
+
+	def deleteTelephone(self, numero):
+		num = Telephone.objects.get(number = numero)
+		num.delete()
+
+	def updateTelephone(self, numberNow, numberNew):
+		num = Telephone.objects.get(number = numberNow)
+		if num != null :
+			num.number=numberNew
+			num.save()
+
 
 	def __str__(self):
 		numero = Telephone.objects.filter(id = Id_number)
