03c2d3f948045049c9140c6651fb2edd6116be2e
gustavo <gustavo@gustavo-HP-G42-Notebook-PC.(none)>
2015-01-23 10:56:11 -0300

    changes

--- Source code/proyectoFinal/telephones/models.py
+++ Source code/proyectoFinal/telephones/models.py
@@ -9,35 +9,7 @@ class LandLine(models.Model):
 	areaCode = models.CharField(max_length=6)
 	Id_number = models.ForeignKey(Telephone)
 
-	def createLandLine(self, numero, area):
-		num = Telephone(number = numero)
-		num.save()
-		fijo = LandLine(Id_number = num.id, areaCode = area)
-		fijo.save()
-		
-
-	def deleteLandLine(self, numero, area):
-		num = Telephone.objects.get(number = numero, areaCode = area)
-		num.delete()
-
-	def updateLandLine(self, numberNow, numberNew,areaNow, areaNew):
-		num = Telephone.objects.get(number = numberNow)
-		cod = LandLine.objects.get(Id_number = num.id, areaCode = areaNow)
-		
-		if numberNew != "" && areaNew!= "" && num != null && cod != null :
-			cod.areaCode = areaNew
-			num.number = numberNew
-			num.save()
-			cod.save()
-		
-		if numberNew == "" && areaNew!= "" && num != null && cod != null :
-			cod.areaCode = areaNew
-			cod.save()			
-
-		if numberNew != "" && areaNew == "" && num != null && cod != null :
-			num.number = numberNew
-			num.save()
-
+	
 
 
 	def __str__(self):
@@ -51,22 +23,7 @@ class LandLine(models.Model):
 class CellPhone(models.Model):
 	Id_number = models.ForeignKey(Telephone)
 
-	def createCellPhone(self,numero):
-		num = Telephone(number = numero)
-		cell = CellPhone(Id_number = num.id)
-		num.save()
-		cell.save()
-
-	def deleteTelephone(self, numero):
-		num = Telephone.objects.get(number = numero)
-		num.delete()
-
-	def updateTelephone(self, numberNow, numberNew):
-		num = Telephone.objects.get(number = numberNow)
-		if num != null :
-			num.number=numberNew
-			num.save()
-
+	
 
 	def __str__(self):
 		numero = Telephone.objects.filter(id = Id_number)
