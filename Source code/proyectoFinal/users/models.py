d798c3ad183304f70072e33838183a4afe843675
Javier Furlán <javifurlan@hotmail.com>
2015-02-04 17:12:34 -0300

    Modified users/models.py

--- Source code/proyectoFinal/users/models.py
+++ Source code/proyectoFinal/users/models.py
@@ -5,18 +5,18 @@ from proyectoFinal.citys.models import City
 class User(models.Model):
 	COMUN = 'CM'
 	PROPIETARIO = 'PR'
-	firstname = models.CharField(max_length=40)
-	lastname =  models.CharField(max_length=40)
+	firstname = models.CharField(max_length=40, verbose_name='Nombre')
+	lastname =  models.CharField(max_length=40, verbose_name='Apellido')
 	email = models.EmailField(unique=True)
-	username = models.CharField(max_length=60, unique=True)
-	password = models.CharField(max_length=60)
-	telephone = models.OneToOneField(Telephone, unique=False)
-	city = models.ForeignKey(City)
+	username = models.CharField(max_length=60, unique=True, verbose_name='Nombre de usuario')
+	password = models.CharField(max_length=60, verbose_name='Contrasena')
+	telephone = models.OneToOneField(Telephone, unique=False, verbose_name='Telefono')
+	city = models.ForeignKey(City, verbose_name='Ciudad')
 	userType_choices = (
 		(COMUN, 'Usuario comun'),
 		(PROPIETARIO, 'Propietario de complejo')
 		)
-	userType = models.CharField(max_length=23, choices=userType_choices, default=COMUN)
+	userType = models.CharField(max_length=23, choices=userType_choices, default=COMUN, verbose_name='Tipo de usuario')
 
 	def __str__(self):
 		return ' %s %s' % (self.first_name, self.last_name)
