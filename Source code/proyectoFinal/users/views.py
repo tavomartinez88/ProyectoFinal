6cd8ddeb1d45bfd3363831397e07c7bdcdc9f9c4
Javier Furlán <javifurlan@hotmail.com>
2015-02-05 10:30:40 -0300

    Added delete user

--- Source code/proyectoFinal/users/views.py
+++ Source code/proyectoFinal/users/views.py
@@ -3,9 +3,23 @@ from django.shortcuts import render_to_response
 from models import User, Telephone
 from forms import UserForm, TelephoneForm
 from django.http import HttpResponseRedirect
-from django.views.generic import ListView, UpdateView, DetailView
+from django.views.generic import ListView, UpdateView, DeleteView
 from django.core.context_processors import csrf # to increase security in the page
 from django.template import RequestContext
+
+def register(request):
+	if request.POST:
+		form = UserForm(request.POST) #create a UserForm
+		tform = TelephoneForm(request.POST)
+		if form.is_valid() and tform.is_valid(): #if the information in the form its correct
+			user = form.save(commit=False) #first prepare the user model, but dont commit it yet to the database
+			user.telephone = tform.save() #add the telephone id to the user model, and save the telephone model
+			user.save() #save the user model
+			return HttpResponseRedirect('/users')
+	else:
+		form = UserForm()
+		tform = TelephoneForm()
+	return render_to_response('users/register.html', {'form': form, 'tform': tform}, RequestContext(request, {}))
 
 class listUser(ListView):
 	template_name = 'users/listUsers.html'
@@ -34,16 +48,6 @@ class telephoneUpdate(UpdateView):
 	template_name_suffix = '_update_form'
 	success_url = '/update_user/1' #redirect when the edit form is filled
 
-def register(request):
-	if request.POST:
-		form = UserForm(request.POST) #create a UserForm
-		tform = TelephoneForm(request.POST)
-		if form.is_valid() and tform.is_valid(): #if the information in the form its correct
-			user = form.save(commit=False) #first prepare the user model, but dont commit it yet to the database
-			user.telephone = tform.save() #add the telephone id to the user model, and save the telephone model
-			user.save() #save the user model
-			return HttpResponseRedirect('/users')
-	else:
-		form = UserForm()
-		tform = TelephoneForm()
-	return render_to_response('users/register.html', {'form': form, 'tform': tform}, RequestContext(request, {}))
+class deleteUser(DeleteView):
+	model = User
+	success_url = '/users'
