d70325e151a8dc43fabf6596762232fe0a812b37
Javier Furlán <javifurlan@hotmail.com>
2015-03-06 11:26:37 -0300

    Some changes

--- Source code/proyectoFinal/complexes/models.py
+++ Source code/proyectoFinal/complexes/models.py
@@ -3,11 +3,11 @@ from django.db import models
 from proyectoFinal.users.models import User
 
 class Complex(models.Model):
-	name = models.CharField(max_length=25)
+	name = models.CharField(max_length=25, verbose_name='Nombre')
 	streetAddress = models.CharField(max_length=25,verbose_name='Direccion')
-	roaster = models.BooleanField()
-	buffet = models.BooleanField()
-	lockerRoom = models.BooleanField()
+	roaster = models.BooleanField(verbose_name='Asador')
+	buffet = models.BooleanField(verbose_name='Cantina')
+	lockerRoom = models.BooleanField(verbose_name='Vestuarios')
 	user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE,verbose_name='Id del usuario')
   
 	def __str__(self):
