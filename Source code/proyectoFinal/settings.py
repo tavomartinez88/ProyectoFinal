113a18a7785c218c46d6316d9df40bb483466c53
gustavo <gmartinezgranella@gmail.com>
2016-11-14 23:37:47 -0300

    se configuro la app para correr sobre sqlite

--- Source code/proyectoFinal/settings.py
+++ Source code/proyectoFinal/settings.py
@@ -75,12 +75,8 @@ WSGI_APPLICATION = 'proyectoFinal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE':'django.db.backends.mysql',
-        'NAME': 'db_tesis',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'ENGINE':'django.db.backends.sqlite3',
+        'NAME': 'db_tesis'
     }
 }
 
